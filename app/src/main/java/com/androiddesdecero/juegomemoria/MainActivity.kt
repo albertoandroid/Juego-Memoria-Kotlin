df236d8a0eda11fe932f5ce52475e026493bac60
Alberto <info@androiddesdecero.com>
2018-03-08 17:04:40 +0100

    Tercer

--- app/src/main/java/com/androiddesdecero/juegomemoria/MainActivity.kt
+++ app/src/main/java/com/androiddesdecero/juegomemoria/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,16 +36,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1.- SetUp Ui
         setUpUi()
+
+        // 2.- Cargar Cartas
+        cargarCartas()
+
+        // 3.- Barajar las Cartas
+        Collections.shuffle(cartas)
+
+        // 4.- setUp OnClickListener
 
     }
 
     private fun setUpUi(){
         mainActivityTvPlayer1.setTextColor(Color.GREEN)
         mainActivityTvPlayer1.setTypeface(null, Typeface.BOLD)
-
         mainActivityTvPlayer2.setTextColor(Color.GRAY)
         mainActivityTvPlayer2.setTypeface(null, Typeface.NORMAL)
+    }
 
+    private fun cargarCartas(){
+        image11 = R.drawable.ic_bike
+        image12 = R.drawable.ic_boat
+        image13 = R.drawable.ic_car
+        image14 = R.drawable.ic_flight
+        image15 = R.drawable.ic_railway
+
+        image21 = R.drawable.ic_bike
+        image22 = R.drawable.ic_boat
+        image23 = R.drawable.ic_car
+        image24 = R.drawable.ic_flight
+        image25 = R.drawable.ic_railway
     }
 }
